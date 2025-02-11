3bebcea3461a739ae26a1224e6f3f72c8e2d62b4
seonghoon <shee5032@naver.com>
2025-02-11 14:03:29 +0900

    chore: eslint 설정

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+const js = require("@eslint/js");
+
+const globals = require("globals");
+
+module.exports = [
+  { ignores: ["dist"] },
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parserOptions: {
+        ecmaVersion: "latest",
+        ecmaFeatures: { jsx: true },
+        sourceType: "module",
+      },
+    },
+    settings: {},
+    plugins: {},
+    rules: {
+      ...js.configs.recommended.rules,
+    },
+  },
+];
