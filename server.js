1c64a3384a53ed3f15ff6df5d6d171ee2751ac0f
seonghoon <shee5032@naver.com>
2025-02-13 14:18:12 +0900

    chore: cors 설정

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 
 const crawlRouter = require("./routes/crawlRoute");
-
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
 
 app.use("/crawl", crawlRouter);
 
