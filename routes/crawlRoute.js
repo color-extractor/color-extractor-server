3519eab2d75be66b094388d4041f45ac081a8726
seonghoon <shee5032@naver.com>
2025-03-03 22:08:21 +0900

    deploy: 팀프로젝트 크롤링서버 docker포함

--- routes/crawlRoute.js
+++ routes/crawlRoute.js
@@ -1,9 +1,18 @@
 const express = require("express");
 
 const { getCrawling } = require("../services/crawlingService");
+const { getCrawlingTitle } = require("../services/crawlingTitleService");
+const { getCrawlingKeyword } = require("../services/crawlingKeywordService");
+const {
+  getCrawlingContentKeyword,
+} = require("../services/crawlingContentService");
 
 const router = express.Router();
 
 router.get("/:url", getCrawling);
+
+router.get("/:url/search", getCrawlingKeyword);
+router.get("/:url/all/search", getCrawlingContentKeyword);
+router.get("/:url/title/search", getCrawlingTitle);
 
 module.exports = router;
---
+++ services/crawlingContentService.js
@@ -0,0 +1,91 @@
+const puppeteer = require("puppeteer");
+
+const getCrawlingContentKeyword = async (req, res) => {
+  const decodedUrl = decodeURIComponent(req.params.url);
+  const keyword = req.query.keyword;
+  const browser = await puppeteer.launch({ headless: true });
+  const TIMEOUT = 20000;
+
+  try {
+    const page = await browser.newPage();
+    await page.goto(decodedUrl);
+
+    const title = await page.$eval("title", (element) => element.textContent);
+    let innerText = await page.$eval("body", (body) => body.innerText);
+
+    const upperCasedKeyword = keyword.toUpperCase();
+    const hasTitleKeyword = title.toUpperCase().includes(upperCasedKeyword);
+    let hasKeyword = innerText.toUpperCase().includes(upperCasedKeyword);
+
+    if (!innerText) {
+      await page.waitForSelector("iframe", { timeout: TIMEOUT });
+
+      const iframeUrl = await page.$eval("iframe", (iframe) => iframe.src);
+      await page.goto(iframeUrl);
+
+      const hasiframeUrlOfNaver = iframeUrl.startsWith(
+        "https://blog.naver.com"
+      );
+      innerText = await page.evaluate(() => document.body.innerText);
+      hasKeyword = innerText.toUpperCase().includes(upperCasedKeyword);
+
+      if (!iframeUrl || !hasiframeUrlOfNaver) {
+        throw new Error(`[Invalid iframe URL]`);
+      }
+    }
+
+    const urlText = getAllSentence(innerText).find((sentence) =>
+      sentence.toUpperCase().includes(upperCasedKeyword)
+    );
+
+    if (hasKeyword) {
+      return res.status(200).json({
+        url: req.params.url,
+        hasTitleKeyword: hasTitleKeyword,
+        hasKeyword: hasKeyword,
+        urlTitle: title,
+        urlText: urlText,
+      });
+    } else {
+      return res.status(200).send({ message: `[This keyword does not exist]` });
+    }
+  } catch (error) {
+    if (!isCheckTrueThisUrl(decodedUrl)) {
+      return res
+        .status(400)
+        .send({ message: `[Invalid Characters in HTTP request]  ${error}` });
+    } else {
+      return res
+        .status(500)
+        .send({ message: `[ServerError occured] ${error}` });
+    }
+  } finally {
+    await browser.close();
+  }
+};
+
+const isCheckTrueThisUrl = (url) => {
+  /* eslint-disable */
+  const urlRegex = /^(http|https):\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/;
+  if (urlRegex.test(url)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const getAllSentence = (innerText) => {
+  return innerText
+    .replace(/\n|\r|\t/g, " ")
+    .split(/(?<=다\. |요\. |니다\. |\. |! |\? )/)
+    .reduce((array, sentence) => {
+      const trimedSentence = sentence.trim();
+
+      if (trimedSentence) {
+        array.push(trimedSentence);
+      }
+      return array;
+    }, []);
+};
+
+module.exports = { getCrawlingContentKeyword };
